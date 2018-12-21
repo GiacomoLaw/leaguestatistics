910c3d6d11cd1040c6ce6026052a99c7aadcdf8d
Giacomo Lawrance <giacomolawrance@gmail.com>
2018-12-21 19:08:50 +0000

    Added main program and gitignore update

---
+++ lolstats.py
@@ -0,0 +1,29 @@
+from riotwatcher import RiotWatcher
+import apisettings
+
+# yourapikey = input('Input your API key: ')
+sumname = input('Your summoner name: ')
+
+watcher = RiotWatcher(apisettings.yourapikey)
+
+my_region = 'euw1'
+
+me = watcher.summoner.by_name(my_region, sumname)
+
+my_ranked_stats = watcher.league.positions_by_summoner(my_region, me['id'])
+# print(my_ranked_stats)
+
+for element in my_ranked_stats:
+	if element['queueType'] == 'RANKED_SOLO_5x5':
+		current_rank = (element['tier']) + '  ' + (element['rank'])
+		points = (element['leaguePoints'])
+		wins = (element['wins'])
+		losses = (element['losses'])
+		total_games = wins + losses
+		rate = round(wins*100/total_games,2)
+		print('Current rank: ', current_rank, '| League Points: ', points)
+		print('Wins: ', wins, '| Losses: ', losses, '| Total games: ', total_games)
+		print('Win rate: ', rate, '%')
+		break
+else:
+	print('Error - no data found')
