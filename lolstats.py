c137f47c547cffd3f6bbd4b7653111adc955d13f
Giacomo Lawrance <giacomolawrance@gmail.com>
2018-12-21 19:15:39 +0000

    pep8 updates

--- lolstats.py
+++ lolstats.py
@@ -1,7 +1,6 @@
 from riotwatcher import RiotWatcher
 import apisettings
 
-# yourapikey = input('Input your API key: ')
 sumname = input('Your summoner name: ')
 
 watcher = RiotWatcher(apisettings.yourapikey)
@@ -12,6 +11,7 @@ me = watcher.summoner.by_name(my_region, sumname)
 
 my_ranked_stats = watcher.league.positions_by_summoner(my_region, me['id'])
 # print(my_ranked_stats)
+# ^ only enabled for debugging
 
 for element in my_ranked_stats:
 	if element['queueType'] == 'RANKED_SOLO_5x5':
@@ -20,7 +20,7 @@ for element in my_ranked_stats:
 		wins = (element['wins'])
 		losses = (element['losses'])
 		total_games = wins + losses
-		rate = round(wins*100/total_games,2)
+		rate = round(wins*100/total_games, 2)
 		print('Current rank: ', current_rank, '| League Points: ', points)
 		print('Wins: ', wins, '| Losses: ', losses, '| Total games: ', total_games)
 		print('Win rate: ', rate, '%')
