2d1a4d2f2e46e33f90c724f163717029bad125a3
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-01-03 18:27:05 +0000

    check for api file (#2) (#3)
    * ignore apisettings.py
    
    api key in it is expired
    
    * minor updates
    
    * added base file checking
    
    * added a checker for api file (#2)

--- lolstats.py
+++ lolstats.py
@@ -1,29 +1,31 @@
-from riotwatcher import RiotWatcher
-import apisettings
+def runmain():
+	from riotwatcher import RiotWatcher
+	import apisettings
 
-sumname = input('Your summoner name: ')
+	sumname = input('Your summoner name: ')
 
-watcher = RiotWatcher(apisettings.yourapikey)
+	watcher = RiotWatcher(apisettings.yourapikey)
 
-my_region = 'euw1'
+	# to do - region select
+	my_region = 'euw1'
 
-me = watcher.summoner.by_name(my_region, sumname)
+	me = watcher.summoner.by_name(my_region, sumname)
 
-my_ranked_stats = watcher.league.positions_by_summoner(my_region, me['id'])
-# print(my_ranked_stats)
-# ^ only enabled for debugging
+	my_ranked_stats = watcher.league.positions_by_summoner(my_region, me['id'])
+	# print(my_ranked_stats)
+	# ^ only enabled for debugging and bui
 
-for element in my_ranked_stats:
-	if element['queueType'] == 'RANKED_SOLO_5x5':
-		current_rank = (element['tier']) + '  ' + (element['rank'])
-		points = (element['leaguePoints'])
-		wins = (element['wins'])
-		losses = (element['losses'])
-		total_games = wins + losses
-		rate = round(wins*100/total_games, 2)
-		print('Current rank: ', current_rank, '| League Points: ', points)
-		print('Wins: ', wins, '| Losses: ', losses, '| Total games: ', total_games)
-		print('Win rate: ', rate, '%')
-		break
-else:
-	print('Error - no data found')
+	for element in my_ranked_stats:
+		if element['queueType'] == 'RANKED_SOLO_5x5':
+			current_rank = (element['tier']) + '  ' + (element['rank'])
+			points = (element['leaguePoints'])
+			wins = (element['wins'])
+			losses = (element['losses'])
+			total_games = wins + losses
+			rate = round(wins*100/total_games, 2)
+			print('Current rank: ', current_rank, '| League Points: ', points)
+			print('Wins: ', wins, '| Losses: ', losses, '| Total games: ', total_games)
+			print('Win rate: ', rate, '%')
+			break
+	else:
+		print('Error - no data found - check server, and summoner name.')
