8662a61fb9b5f8ee672bf1a7c87b292c49cb601e
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-01-03 18:25:35 +0000

    added a checker for api file (#2)

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 from pathlib import Path
+import msvcrt as m
 import lolstats
+
+
+def wait():
+	m.getch()
+
 
 apisettingspath = Path("apisettings.py")
 
-if apisettingspath.is_file():
-    print("API file present")
-    lolstats
+if apisettingspath.exists():
+	print("API file present - note that the API key expires every 24 hours")
+	lolstats.runmain()
 else:
-	print("No API file")
+	print("No API file - press any key to create")
+	createfile = open("apisettings.py", "w+")
+	createfile.write("yourapikey = ")
+	uapi = input("What is your API key? ")
+	createfile = open("apisettings.py", "a+")
+	createfile.write("'%s" % uapi)
+	createfile.write("'")
+	createfile.close()
+	print("API file created")
+	lolstats.runmain()
