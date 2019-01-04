dd558ffad147ac70d523478a37ad1e828058ff3d
Giacomo Lawrance <giacomolawrance@gmail.com>
2019-01-04 10:57:45 +0000

    display server list from json file

--- lib/functions.py
+++ lib/functions.py
@@ -1,6 +1,16 @@
 import msvcrt as m
-import json
+import requests
 
 
 def waitforkey():
 	m.getch()
+
+
+def serverselect():
+	url = "https://raw.githubusercontent.com/GiacomoLaw/lolstats/master/lib/serverlist.json"
+	data = requests.get(url).json()
+	for index, element in enumerate(data, start=1):
+		print("{}. {}".format(index, element['richname']))
+
+
+print(serverselect())
